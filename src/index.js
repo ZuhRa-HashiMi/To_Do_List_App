87149de1af88fcd812bb5d74f18c378a31ff3b4e
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-11-11 12:42:41 +0430

    add index.js file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+const list = document.querySelector('.list');
+const inputTodo = document.querySelector('.add-todo');
+const addTodoBtn = document.querySelector('.submit');
+
+class TodoTask {
+  constructor(desp, arr, done = false) {
+    this.desp = desp;
+    this.index = arr.length;
+    this.done = done;
+  }
+}
